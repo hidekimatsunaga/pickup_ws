8907cf78baf28498b75989cf9ae63b6c192bb7a9
hidekimatsunaga <hide.matsuhide0312@gmail.com>
2025-12-27 16:57:52 +0900

    feat: air_thresholdの値を-120.0に変更し、stop_angles_に新しい角度を追加

--- src/hose_control/include/hose_control/motor_initial_position.hpp
+++ src/hose_control/include/hose_control/motor_initial_position.hpp
@@ -1,4 +1,4 @@
 #include <vector>
 
-std::vector<float> stop_angles_ = {328.97f, 318.60f, 222.63f, 181.58f, 66.80f, 72.25f, 211.64f, 248.03f, 95.71f};
+std::vector<float> stop_angles_ = {328.97f, 318.60f, 222.63f, 181.58f, 66.80f, 72.25f, 211.64f, 248.03f, 95.71f, 196.00f};
 float stop_motor10_angle_ = 196.00f;
